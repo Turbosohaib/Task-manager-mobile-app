d1382c60bfba189f6f60df8b4e6747d661997a59
Sohaib Shah <turbosohaib1234@gmail.com>
2023-09-18 14:03:59 +0500

    fixed UI

--- pages/component/sidebar.js
+++ pages/component/sidebar.js
@@ -27,63 +27,63 @@ function Sidebar() {
                     </svg>
                 </div>
             </div>
+        </div>
 
-            <div className={`${openSideBar ? "fixed inset-0 bg-black bg-opacity-75 transition-opacity z-20" : ""} `}>
-                <aside className={`fixed inset-y-0 left-0 bg-white shadow-md max-h-screen  ease-in-out duration-300 ${openSideBar ? "w-[190px]" : "w-0"}  `}>
-                    <div className={`flex flex-col  justify-between h-full border-l-4 border-blue-400 rounded-l ${openSideBar ? "" : "hidden"}`}>
-                        <div className="flex-grow">
-                            <div className="flex items-center px-4 py-6 text-center border-b">
-                                <svg xmlns="http://www.w3.org/2000/svg" onClick={() => setOpenSideBar(!openSideBar)} className="px-3 mt-[4px] cursor-pointer" height="20px" viewBox="0 0 448 512" opacity="0.5">
-                                    <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
-                                </svg>
-                                <h1 className="text-md font-semibold leading-none"><span className="text-blue-600/75">Task Manager</span></h1>
-                            </div>
-                            <div className="p-4">
-                                <ul className="space-y-1">
-                                    <li>
-                                        <a href="/" id="sideBarTracker" className="flex bg-gray-100 items-center hover:bg-yellow-50 rounded-xl font-bold text-sm py-3 px-4">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-2">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                            </svg>
-                                            <p >Time Tracker</p>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <Link href="#" className="flex bg-gray-100 hover:bg-yellow-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
-                                                <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5 4h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm0 2h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1z" />
-                                            </svg>Task list
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <a href="#"
-                                            className="flex bg-gray-100 hover:bg-yellow-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
-                                                <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z" />
-                                            </svg>Projects
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" className="flex bg-gray-100 hover:bg-yellow-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
-                                                <path d="M2 1a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l4.586-4.586a1 1 0 0 0 0-1.414l-7-7A1 1 0 0 0 6.586 1H2zm4 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
-                                            </svg>Tags
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
+        <div className={`${openSideBar ? "fixed inset-0 bg-black bg-opacity-75 transition-opacity z-20" : ""} `}>
+            <aside className={`fixed inset-y-0 left-0 bg-white shadow-md max-h-screen  ease-in-out duration-300 ${openSideBar ? "w-[190px]" : "w-0"}  `}>
+                <div className={`flex flex-col  justify-between h-full border-l-4 border-blue-400 rounded-l ${openSideBar ? "" : "hidden"}`}>
+                    <div className="flex-grow">
+                        <div className="flex items-center px-4 py-6 text-center border-b">
+                            <svg xmlns="http://www.w3.org/2000/svg" onClick={() => setOpenSideBar(!openSideBar)} className="px-3 mt-[4px] cursor-pointer" height="20px" viewBox="0 0 448 512" opacity="0.5">
+                                <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+                            </svg>
+                            <h1 className="text-md font-semibold leading-none"><span className="text-blue-600/75">Task Manager</span></h1>
                         </div>
                         <div className="p-4">
-                            <Link href='/api/auth/signout'
-                                className="inline-flex items-center justify-center h-9 px-4 rounded-xl bg-gray-900 text-gray-300 hover:text-white text-sm font-semibold transition">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="" viewBox="0 0 16 16">
-                                    <path d="M12 1a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2a1 1 0 0 1 1-1h8zm-2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
-                                </svg>
-                            </Link> <span className="font-bold text-sm ml-2">Logout</span>
+                            <ul className="space-y-1">
+                                <li>
+                                    <a href="/" id="sideBarTracker" className="flex bg-gray-100 items-center hover:bg-yellow-50 rounded-xl font-bold text-sm py-3 px-4">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-2">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+                                        <p >Time Tracker</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <Link href="#" className="flex bg-gray-100 hover:bg-yellow-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
+                                            <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5 4h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm0 2h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1z" />
+                                        </svg>Task list
+                                    </Link>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        className="flex bg-gray-100 hover:bg-yellow-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
+                                            <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z" />
+                                        </svg>Projects
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" className="flex bg-gray-100 hover:bg-yellow-50 rounded-xl font-bold text-sm text-gray-900 py-3 px-4">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="text-lg mr-4" viewBox="0 0 16 16">
+                                            <path d="M2 1a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l4.586-4.586a1 1 0 0 0 0-1.414l-7-7A1 1 0 0 0 6.586 1H2zm4 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                                        </svg>Tags
+                                    </a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
-                </aside>
-            </div>
+                    <div className="p-4">
+                        <Link href='/api/auth/signout'
+                            className="inline-flex items-center justify-center h-9 px-4 rounded-xl bg-gray-900 text-gray-300 hover:text-white text-sm font-semibold transition">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="" viewBox="0 0 16 16">
+                                <path d="M12 1a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2a1 1 0 0 1 1-1h8zm-2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                            </svg>
+                        </Link> <span className="font-bold text-sm ml-2">Logout</span>
+                    </div>
+                </div>
+            </aside>
         </div>
     </>
 }
